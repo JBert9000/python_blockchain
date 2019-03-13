0e3a14171f9462b264913158bc2ad07e50fe6a52
John <jbertlik@gmail.com>
2019-03-13 17:48:54 -0400

    Been following the tutorial and created end points as well as files that store transactions. There is an error in my code that I will address. More to come.

--- blockchain.py
+++ blockchain.py
@@ -1,38 +1,129 @@
+import hashlib
+import json
+from textwrap import dedent
+from time import time
+from uuid import uuid4
+
+from flask import Flask
+
 class Blockchain(object):
+
+    # Instantiate our Node
+    app = Flask(__name__)
+
+    # Generate a globally unique address for this node
+    node_identifier = str(uuid4()).replace('-', '')
+
+    # Instantiate the Blockchain
+    blockchain = Blockchain()
+
+
+@app.route('/mine', methods = ['GET'])
+def mine():
+    return "Let's mine a new block baby"
+
+@app.route('/transactions/new', methods = ['POST'])
+def new_trasaction():
+    return "Let's add a new transaction"
+
+@app.route('/chain', methods['GET'])
+def full_chain():
+    response = {
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain),
+    }
+    return jsonify(response), 200
+
+if __name__ == '__main__':
+    app.run(host = '0.0.0.0', port = 5000)
+
     def __init__(self):
+        self.current_transactions = []
         self.chain = []
+
+        # Creates genesis block
+        self.new_block(previous_hash= 1, proof= 100)
+
+    def proof_of_work(self, last_proof):
+        """
+        Simple Proof of Work Algorithm:
+         - Find a number p' such that hash(pp') contains leading 4 zeroes, where p is the previous p'
+         - p is the previous proof, and p' is the new proof
+        :param last_proof: <int>
+        :return: <int>
+        """
+
+        proof = 0
+        while self.valid_proof(last_proof, proof) is False:
+            proof += 1
+
+        return proof
+
+    @staticmethod
+    def valid_proof(last_proof, proof):
+        """
+        Validates the Proof: Does hash(last_proof, proof) contain 4 leading zeroes?
+        :param last_proof: <int> Previous Proof
+        :param proof: <int> Current Proof
+        :return: <bool> True if correct, False if not.
+        """
+
+        guess = f'{last_proof}{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:4] == "0000"
+
+    def new_block(self, proof, previous_hash=None):
+        """
+        Create a new Block in the Blockchain
+        :param proof: <int> The proof given by the Proof of Work algorithm
+        :param previous_hash: (Optional) <str> Hash of previous Block
+        :return: <dict> New Block
+        """
+
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1])
+        }
+
+        # reset the current list of transactions
         self.current_transactions = []
 
-
-    def new_blockchain(self):
-        # Creasts a new Block and adds it to the chain
-        pass
-
+        self.chain.append(block)
+        return block
 
     def new_transaction(self, sender, recipient, amount):
-        # Adds a new transation to the list of transactions
-        ""
+        """
+        Creates a new transaction to go into the next mined Block
         :param sender: <str> Address of the Sender
         :param recipient: <str> Address of the Recipient
         :param amount: <int> Amount
-        :return: <int> The index of the block that will hold this transaction
-        ""
-
+        :return: <int> The index of the Block that will hold this transaction
+        """
         self.current_transactions.append({
             'sender': sender,
             'recipient': recipient,
-            'amount': amount
+            'amount': amount,
         })
 
         return self.last_block['index'] + 1
 
-    @staticmethod
-    def hash(block):
-        # Hashes a Block
-        pass
-
     @property
     def last_block(self):
         # Returns the last block in the chain
-        pass
+        return self.chain[-1]
 
+    @staticmethod
+    def hash(block):
+        # Hashes a Block
+        """
+        Creates a SHA-256 hash of a Block
+        :param block: <dict> Block
+        :return: <str>
+        """
+
+        # Must make sure that the dictionary is ordered, or else we'll have inconsistent hashes
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
